fbd42b931a7078ab17b1eac2ecf60ad966a7ddab
DifficultIV <kitpisan@gmail.com>
2020-12-15 15:48:20 +0700

    Commit Message "lab5_1.cpp completed"

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int n;
+    int odd=0;
+    int even=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin >> n;
+    while(n != 0){
+if(n % 2 == 0){
+    even ++;
 }
+else{
+    odd ++;
+}
+    cout << "Enter an integer: ";
+    cin >> n;
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
+    return 0;}
